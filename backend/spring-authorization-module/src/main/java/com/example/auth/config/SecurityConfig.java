b7e977fa3c3e0fda56b5686ef468ca6f2fd893a3
ìMyLittleChicken <jackbomb51@gmail.com>
2025-04-19 14:57:37 +0900

    chore: 설정파일 작성
    1. SecurityConfig
    2. PasswordEncoderConfig
    3. application.yml
    4. build.gradle

---
+++ backend/spring-authorization-module/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.example.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+                new OAuth2AuthorizationServerConfigurer();
+
+        http
+                .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+                .with(authorizationServerConfigurer, (configurer) -> configurer
+                        .oidc(Customizer.withDefaults()) // OpenID Connect 사용
+                )
+                // 인증이 필요한 모든 요청 허용
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                // formLogin 같은 기본 로그인 페이지 비활성화
+                .formLogin(AbstractHttpConfigurer::disable)
+                // 인증 실패 시 기본 로그인 페이지로 리다이렉트하지 않도록 설정
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/oauth2/**").permitAll() // OAuth2 관련 엔드포인트는 인증 없이 접근 가능
+                        .anyRequest().authenticated()
+                )
+                // spring security의 기본 로그인 페이지를 사용하지 않도록 설정
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        return http.cors(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("http://127.0.0.1:3000");
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+}
